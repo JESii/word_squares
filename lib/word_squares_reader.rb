df46238a2a334c72482ac302e6454fee0276ed4d
Jon Seidel <jseidel@edpci.com>
2013-12-03 14:50:24 -0800

    Reading word file...
    Can get simple word lists loaded.

--- lib/word_squares_reader.rb
+++ lib/word_squares_reader.rb
@@ -1,8 +1,16 @@
 class WordSquaresReader
   def initialize(filename)
-    @filename = filename
+    @filename = checked_for_input_filename(filename)
   end
+
   def getwords(count)
-    ['a','i']
+    @filename.each_line.map { |w| w.chomp.size == count ? w.chomp : nil }.compact
+  end
+
+  private
+
+  def checked_for_input_filename(filename)
+    raise "Invalid word file" if File.readable?(filename) == false
+    File.open(filename)
   end
 end
--- spec/word_squares_reader_spec.rb
+++ spec/word_squares_reader_spec.rb
@@ -2,8 +2,15 @@ require 'rspec'
 require_relative '../lib/word_squares_reader'
 
 describe "WordSquaresReader" do
+  it "aborts with an invalid input file" do
+    expect{ WordSquaresReader.new('junque') }.to raise_error    #("Invalid word file")
+  end
   it "loads a word file successfully" do
     wsr = WordSquaresReader.new('one_letter_words.txt')
     expect(wsr.getwords(1)).to eql ['a','i']
+  end
+  it "load two-letter words successfully" do
+    wsr = WordSquaresReader.new('two_letter_words.txt')
+    expect(wsr.getwords(2)).to eql ['as', 'am', 'no', 'me']
   end
 end
