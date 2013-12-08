0f6a6a091ee3c8f9654c70525f7322524ba6a0ce
JESii <jseidel@edpci.com>
2013-12-08 05:11:41 -0800

    Add Square class for the square itself

---
+++ lib/squares.rb
@@ -0,0 +1,47 @@
+class Square
+  def initialize(dimension)
+    @dimension = dimension
+    @rows = @cols = @dimension -1
+    @square = Array.new(dimension)
+    (0..@rows).each do |r|
+      @square[r] = " " * @dimension
+    end
+    @all_rows = @all_cols = (0..@rows)
+  end
+
+  def size
+    @dimension
+  end
+
+  def set_row(row,value)
+    @square[row-1] = value
+  end
+  alias :[]= :set_row
+  def get_row(row)
+    @square[row-1]
+  end
+  alias :row :get_row
+  alias :[] :get_row
+
+  def col(col)
+    col_word = ''
+    @all_rows.each do |r|
+      col_word << @square[r][col-1]
+    end
+    col_word.rstrip
+  end
+  def pivot_on_point(point)
+    pt = point -1
+    (point..@rows).each do |r|
+      @square[r][pt] = @square[pt][r]
+    end
+  end
+  def clear_on_pivot(point)
+    pt = point - 1
+    (point..@rows).each do |r|
+      (point..@cols).each do |c|
+        @square[r][pt] = ' '
+      end
+    end
+  end
+end
---
+++ spec/squares_spec.rb
@@ -0,0 +1,44 @@
+require 'rspec'
+require 'spec_helper'
+require_relative '../lib/squares'
+
+
+puts '===============Squares==================='
+
+describe Square do
+  it "creates a new square" do
+    s = Square.new(2)
+    expect(s.class).to eql Square
+    expect(s.size).to eql 2
+  end
+  it "adds a new word row" do
+    s = Square.new(2)
+    s.set_row(1,'an')
+    s[2]='no'
+    expect(s.get_row(1)).to eq 'an'
+    expect(s[2]).to eq 'no'
+  end
+  it "returns a column" do
+    s = Square.new(2)
+    s[1] = 'an'
+    s[2] = 'ab'
+    expect(s.col(1)).to eq 'aa'
+    expect(s.col(2)).to eq 'nb'
+  end
+  it "assigns a row pivot" do
+    s = Square.new(3)
+    s[1] = 'abc'
+    s.pivot_on_point(1)
+    expect(s.col(1)).to eq 'abc'
+  end
+  it "clears a row pivot" do
+    s = Square.new(3)
+    s[1] = 'abc'
+    s.pivot_on_point(1)
+    expect(s.col(1)).to eq 'abc'
+    s.clear_on_pivot(1)
+    expect(s.col(1)).to eq 'a'
+    expect(s.col(2)).to eq 'b'
+    expect(s.col(3)).to eq 'c'
+  end
+end
