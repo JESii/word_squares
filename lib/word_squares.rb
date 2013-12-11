53e1bda0fa57033a233192d3ca43145a005d1349
JESii <jseidel@edpci.com>
2013-12-11 14:20:38 -0800

    Refactor strategy driver...
    Extract method checked_strategy() to handle different strategy inputs/types
    Accept strings or recognized classes or set default if nil.

--- lib/word_squares.rb
+++ lib/word_squares.rb
@@ -1,5 +1,6 @@
 require_relative 'word_squares_reader'
 require_relative 'base_word_squares'
+require_relative 'pivot_word_squares'
 
 class WordSquares
   attr_accessor :square, :word_list
@@ -10,6 +11,7 @@ class WordSquares
   end
 
   def generate(dimension, strategy=BaseWordSquares)
+    strategy = checked_strategy(strategy)
     @dimension = dimension
     wsr = WordSquaresReader.new(@filename)
     return [] if @dimension == 1
@@ -19,5 +21,20 @@ class WordSquares
     ws = strategy.new(@dimension, @word_list)
     word_square =  ws.select_square
     word_square.to_s
+  end
+
+  def checked_strategy(strategy)
+    case
+    when strategy.nil?
+      BaseWordSquares
+    when strategy.class == String
+      Kernel.const_get(strategy)
+    when strategy == BaseWordSquares
+      strategy
+    when strategy == PivotWordSquares
+      strategy
+    else
+      raise "Invalid strategy option: #{strategy}"
+    end
   end
 end
